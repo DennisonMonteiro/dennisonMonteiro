3004f0e6a24a08d199285775c5e1caa646e447e7
Dennison Monteiro <dennison.cp2@gmail.com>
2022-02-05 06:08:54 -0300

    fixing anchor href and package.json homepage pt.2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             <ul className='header-items'>
                 <li className='item'>
                     <AiOutlineHome className='item-icon' />
-                    <a href="/portfolio-dennison-monteiro">Home</a>
+                    <a href="/dennisonMonteiro">Home</a>
                 </li>
                 <li className='item'>
                     <GrContactInfo className='item-icon' />
